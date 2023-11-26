3856fc028da14f4d629c3c4ddc1562d0f278d738
Sawssen Gharbi <sawssen.gharbi@esprit.tn>
2023-11-26 20:42:09 +0100

    Commit Test

---
+++ Assets/Plugins/Android/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,3 @@
+
+J
+baseProjectTemplate.gradle,b\b\bb1b1b7c22f5fbf7b9c3bffdcb2f85f38872fd6a
--- Assets/Vibrator.cs
+++ Assets/Vibrator.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class Vibrator 
+public static class Vibratorr
 {
 #if UNITY_ANDROID && !UNITY_EDITOR
 
